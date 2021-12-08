410dcce4e894e3508ccab12938c1598543076c58
Matthew <phillips.matthew829@gmail.com>
2021-12-08 12:08:33 -0600

    commit tree 9c2a7a773a4c1be723c693e373b3c643b12711a1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Cat{
+    constructor(name, sex){ //Create properties.
+        this.name = name;
+        this.sex = sex;
+    } //Create method(s).
+    speak(){
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog{
+    constructor(name, sex){ //Create properties.
+        this.name = name;
+        this.sex = sex;
+    } //Create method(s).
+    speak(){
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird{
+    constructor(name, sex){ //Create properties.
+        this.name = name;
+        this.sex = sex;
+    } //Create method(s).
+    speak(){
+        if(this.sex === "male") return `It\'s me! ${this.name}, the parrot!`;
+        else return `${this.name} says squawk!`;
+    }
+}
